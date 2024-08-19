eb8524d590bdf6f335fbdd4cf20ed543576ba662
Santii05-C <santi.c.pgm@gmail.com>
2024-08-19 12:00:42 -0300

    last commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,44 +2,5 @@ import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return (
-    <div>
-      <Link href="/">
-        <h1>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Hic animi
-          eveniet ipsam harum! Necessitatibus unde dolorem perspiciatis eius sae
-          ad voluptate nihilLorem ipsum dolor sit, amet consectetur adipisicing
-          elit. Hic animi eveniet ipsam harum! Necessitatibus
-        </h1>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Assumenda vero
-        consectetur quae error placeat? Neque odio delectus ducimus saepe
-        perferendis quia sequi exercitationem dignissimos veniam assumenda eos
-        numquam cupiditate impedit obcaecati molestias architecto, modi tenetur!
-        Quo debitis tempora adipisci facere quae molestiae laborum voluptate
-        quas itaque ipsum repellat, laudantium, quia reprehenderit ducimus
-        assumenda nam nisi tenetur sapiente cupiditate quisquam magnam quam
-        beatae. Accusamus, quos rem. Fuga, debitis officiis quasi magni
-        praesentium mollitia corrupti, ea suscipit recusandae consequuntur
-        architecto totam aliquid velit animi ad odit quidem ipsam. Deleniti,
-        quibusdam tempore veniam sed numquam quod neque id, deserunt cupiditate
-        corrupti autem nulla tenetur iste libero nesciunt eos. Voluptatibus
-        dicta, mollitia debitis eveniet minus exercitationem iure eaque delectus
-        fugiat a non libero earum qui atque ea praesentium blanditiis at
-        distinctio. Distinctio nulla facilis quis ratione. Exercitationem et s
-        sit soluta molestiae vesam temporibus dignissimos debitis ut vitae
-        expedita officia quibusdam ducimus, nesciunt doloribus sed commodi sint,
-        culpa distinctio id saepe ratione officiis! Totam quidem culpa veniam
-        eveniet pariatur, officiis est ipsam ipsum eos doloribus sed maiores
-        autem maxime itaque sit esse necessitatibus tempora! Pariatur at sit
-        eius consequuntur nisi placeat laboriosam ad dolorum. consectetur quae
-        error placeat? Neque odio delectus ducimus saepe perferendis quia sequi
-        exercitationem dignissimos veniam assumenda eos numquam cupiditate
-        impedit obcaecati molestias architecto, modi tenetur! Quo debitis
-        tempora adipisci facere quae molestiae laborum voluptate quas itaque
-        ipsum repellat, laudantium, quia reprehenderit ducimus assumenda nam
-        nisi tenetur sapiente cupiditate quisquam magnam quam beatae. Accusamus
-        con
-      </Link>
-    </div>
-  );
+  return <div></div>;
 }
---
+++ src/components/featured/Featued.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import styles from "./featued.module.css";
+
+const Featued = () => {
+  return <div className={styles.container}>Featued</div>;
+};
+
+export default Featued;
