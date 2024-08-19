98efefbbbbbffefb4518d8e5bfa2b274114697de
Santii05-C <santi.c.pgm@gmail.com>
2024-08-19 12:45:18 -0300

    last commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,19 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
+import Featued from "@/components/featured/Featued";
+import CategoryList from "@/components/categoryList/CategoryList";
+import CardList from "@/components/cardList/CardList";
+import Menu from "@/components/Menu/Menu";
 
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className={styles.container}>
+      <Featued />
+      <CategoryList />
+      <div className={styles.container}>
+        <CardList />
+        <Menu />
+      </div>
+    </div>
+  );
 }
---
+++ src/components/themeToggle/ThemeToggle.jsx
@@ -0,0 +1,6 @@
+import styles from "./";
+const ThemeToggle = () => {
+  return <div className={styles.container}>ThemeToggle</div>;
+};
+
+export default ThemeToggle;
