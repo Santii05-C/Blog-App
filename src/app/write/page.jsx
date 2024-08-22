b98ef324f54d8f40fe56196ed046e9249f05eda9
Santii05-C <santi.c.pgm@gmail.com>
2024-08-22 11:41:15 -0300

    last commit

--- src/app/write/page.jsx
+++ src/app/write/page.jsx
@@ -1,14 +1,21 @@
+"use client";
+
 import Image from "next/image";
 import styles from "./write.module.css";
 import { useState } from "react";
+import ReactQuill from "react-quill";
+
+import "react-quill/dist/quill.bubble.css";
 
 const WritePage = () => {
   const [open, setOpen] = useState(false);
+  const [value, setValue] = useState("");
+
   return (
     <div className={styles.container}>
-      <input type="text" placeholder="Title" />
+      <input type="text" placeholder="Title" className={styles.input} />
       <div className={styles.editor}>
-        <button className={styles.button}>
+        <button className={styles.button} onClick={() => setOpen(!open)}>
           <Image src="/plus.png" alt="" width={16} height={16} />
         </button>
         {open && (
@@ -24,7 +31,15 @@ const WritePage = () => {
             </button>
           </div>
         )}
+        <ReactQuill
+          className={styles.textArea}
+          theme="bubble"
+          value={value}
+          onChange={setValue}
+          placeholder="Tell your story..."
+        />
       </div>
+      <button className={styles.publish}>Publish</button>
     </div>
   );
 };
