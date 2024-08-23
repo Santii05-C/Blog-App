041e4de407b84a366cc5e045c4cf0904a9119fe3
Santii05-C <santi.c.pgm@gmail.com>
2024-08-23 11:10:04 -0300

    last commit

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,7 +1,9 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
 import GithubProvider from "next-auth/providers/github";
 import GlogleProvider from "next-auth/providers/google";
 
 export const authOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
     GlogleProvider({
       clientId: process.env.GOOGLE_ID,
