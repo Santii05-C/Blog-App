88d219e97faf9f94cee6fe3751a33473a82b8397
Santii05-C <santi.c.pgm@gmail.com>
2024-08-23 18:04:16 -0300

    last commit

--- src/components/categoryList/CategoryList.jsx
+++ src/components/categoryList/CategoryList.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import styles from "./categoryList.module.css";
 import Link from "next/link";
 import Image from "next/image";
@@ -7,6 +7,8 @@ const getData = async () => {
   const res = await fetch("http://localhost:3000/api/categories", {
     cache: "no-store",
   });
+
+  console.log(res);
 
   if (!res.ok) {
     throw new Error("Failed");
@@ -17,27 +19,30 @@ const getData = async () => {
 
 const CategoryList = async () => {
   const data = await getData();
+  console.log(data);
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Popular Categories</h1>
       <div className={styles.categories}>
-        {data?.map((item) => (
-          <Link
-            href="/blog?cat=style"
-            className={`${styles.category} ${styles[item.slug]}`}
-            key={item._id}
-          >
-            <Image
-              src="/style.png"
-              alt=""
-              width={32}
-              height={32}
-              className={styles.image}
-            />
-            styles
-            {item.title}
-          </Link>
-        ))}
+        {/* {!data && <p>no existe data</p>} */}
+        <Suspense fallback={"falló"}>
+          {data?.map((item) => (
+            <Link
+              href="/blog?cat=style"
+              className={`${styles.category} ${styles[item.slug]}`}
+              key={item._id}
+            >
+              <Image
+                src={item.img}
+                alt=""
+                width={32}
+                height={32}
+                className={styles.image}
+              />
+              {item.title}
+            </Link>
+          ))}
+        </Suspense>
       </div>
     </div>
   );
