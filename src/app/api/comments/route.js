e484bbb2b11a7b39c55afe4fc0a578557f721526
Santii05-C <santi.c.pgm@gmail.com>
2024-08-26 12:41:46 -0300

    last commit

--- src/app/api/comments/route.js
+++ src/app/api/comments/route.js
@@ -1,3 +1,4 @@
+import { getAuthSession } from "@/utils/auth";
 import prisma from "@/utils/connect";
 import { NextResponse } from "next/server";
 
@@ -5,6 +6,33 @@ import { NextResponse } from "next/server";
 export const GET = async (req) => {
   const { searchParams } = new URL(req.url);
   const postSlug = searchParams.get("postSlug");
+
+  try {
+    const comments = await prisma.comment.findMany({
+      where: {
+        ...(postSlug && { postSlug }),
+      },
+      include: { user: true },
+    });
+
+    return new NextResponse(JSON.stringify(comments, { status: 200 }));
+  } catch (err) {
+    console.log(err);
+    return new NextResponse(
+      JSON.stringify({ message: "Something went wrong!" }, { status: 500 })
+    );
+  }
+};
+
+// GET CREATE COMMENT
+export const POST = async (req) => {
+  const session = getAuthSession();
+
+  if (!session) {
+    return new NextResponse(
+      JSON.stringify({ message: "Not Authenticated!" }, { status: 401 })
+    );
+  }
 
   try {
     const comments = await prisma.comment.findMany({
